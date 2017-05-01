52961af88ed4281e77f36565b8c2cc421b3f14de
Logan Rakai <lmrakai@gmail.com>
2017-05-01 12:23:53 -0600

    Adding fireSprinkler script

---
+++ scripts/fireSprinkler.py
@@ -0,0 +1,230 @@
+'''
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import sys
+import logging
+import time
+import getopt
+import json
+
+class FireSprinkler:
+
+    def __init__(self, name):
+        self.name = name
+        self.reading = 0
+        self.state = 'Deactivated' 
+
+    def readingMessage(self):
+        return "Smoke level at " + self.reading
+
+# Shadow JSON schema:
+#
+# Name: Bot
+# {
+#	"state": {
+#		"desired":{
+#			"property":<INT VALUE>
+#		}
+#	}
+#}
+
+# Custom Shadow callback
+
+
+def customShadowCallback_Update(payload, responseStatus, token):
+    # payload is a JSON string ready to be parsed using json.loads(...)
+    # in both Py2.x and Py3.x
+    if responseStatus == "timeout":
+        print("Update request " + token + " time out!")
+    if responseStatus == "accepted":
+        payloadDict = json.loads(payload)
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Update request with token: " + token + " accepted!")
+        print("property: " + str(payloadDict["state"]["desired"]["property"]))
+        print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    if responseStatus == "rejected":
+        print("Update request " + token + " rejected!")
+
+
+def customShadowCallback_Delete(payload, responseStatus, token):
+    if responseStatus == "timeout":
+        print("Delete request " + token + " time out!")
+    if responseStatus == "accepted":
+        print("~~~~~~~~~~~~~~~~~~~~~~~")
+        print("Delete request with token: " + token + " accepted!")
+        print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    if responseStatus == "rejected":
+        print("Delete request " + token + " rejected!")
+
+
+def customShadowCallback_Delta(payload, responseStatus, token):
+        # payload is a JSON string ready to be parsed using json.loads(...)
+        # in both Py2.x and Py3.x
+    print(responseStatus)
+    payloadDict = json.loads(payload)
+    print("++++++++DELTA++++++++++")
+    print("property: " + str(payloadDict["state"]["property"]))
+    print("version: " + str(payloadDict["version"]))
+    print("+++++++++++++++++++++++\n\n")
+
+# Custom MQTT message callback
+
+
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+
+# Usage
+usageInfo = """Usage:
+Use certificate based mutual authentication:
+python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -c <certFilePath> -k <privateKeyFilePath>
+Use MQTT over WebSocket:
+python basicPubSub.py -e <endpoint> -r <rootCAFilePath> -w
+Type "python basicPubSub.py -h" for available options.
+"""
+# Help info
+helpInfo = """-e, --endpoint
+	Your AWS IoT custom endpoint
+-r, --rootCA
+	Root CA file path
+-c, --cert
+	Certificate file path
+-k, --key
+	Private key file path
+-w, --websocket
+	Use MQTT over WebSocket
+-h, --help
+	Help information
+"""
+
+# Read in command-line parameters
+useWebsocket = False
+host = ""
+rootCAPath = ""
+certificatePath = ""
+privateKeyPath = ""
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:", [
+                               "help", "endpoint=", "key=", "cert=", "rootCA=", "websocket"])
+    if len(opts) == 0:
+        raise getopt.GetoptError("No input parameters!")
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(helpInfo)
+            exit(0)
+        if opt in ("-e", "--endpoint"):
+            host = arg
+        if opt in ("-r", "--rootCA"):
+            rootCAPath = arg
+        if opt in ("-c", "--cert"):
+            certificatePath = arg
+        if opt in ("-k", "--key"):
+            privateKeyPath = arg
+        if opt in ("-w", "--websocket"):
+            useWebsocket = True
+except getopt.GetoptError:
+    print(usageInfo)
+    exit(1)
+
+# Missing configuration notification
+missingConfiguration = False
+if not host:
+    print("Missing '-e' or '--endpoint'")
+    missingConfiguration = True
+if not rootCAPath:
+    print("Missing '-r' or '--rootCA'")
+    missingConfiguration = True
+if not useWebsocket:
+    if not certificatePath:
+        print("Missing '-c' or '--cert'")
+        missingConfiguration = True
+    if not privateKeyPath:
+        print("Missing '-k' or '--key'")
+        missingConfiguration = True
+if missingConfiguration:
+    exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTShadowClient
+myAWSIoTMQTTShadowClient = None
+if useWebsocket:
+    myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient(
+        "fire-sprinkler", useWebsocket=True)
+    myAWSIoTMQTTShadowClient.configureEndpoint(host, 443)
+    myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient("fire-sprinkler")
+    myAWSIoTMQTTShadowClient.configureEndpoint(host, 8883)
+    myAWSIoTMQTTShadowClient.configureCredentials(
+        rootCAPath, privateKeyPath, certificatePath)
+
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+# Infinite offline Publish queueing
+myAWSIoTMQTTShadowClient.configureOfflinePublishQueueing(-1)
+myAWSIoTMQTTShadowClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# AWSIoTMQTTShadowClient configuration
+myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Create a device
+device = FireSprinkler("fire-sprinkler")
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTShadowClient.connect()
+myAWSIoTMQTTClient = myAWSIoTMQTTShadowClient.getMQTTConnection()
+myAWSIoTMQTTClient.subscribe("office/kitchen", 1, customCallback)
+time.sleep(2)
+
+# Create a deviceShadow with persistent subscription
+Bot = myAWSIoTMQTTShadowClient.createShadowHandlerWithName(
+    "fire-sprinkler", True)
+
+# Delete shadow JSON doc
+Bot.shadowDelete(customShadowCallback_Delete, 5)
+
+# Listen on deltas
+Bot.shadowRegisterDeltaCallback(customShadowCallback_Delta)
+
+# Update shadow in a loop
+loopCount = 0
+while True:
+    myAWSIoTMQTTClient.publish(
+        "office/kitchen", device.readingMessage(), 1)
+    JSONPayload = '{"state":{"desired":{"sprinkler":' + \
+        'deactivated"}}}'
+    Bot.shadowUpdate(JSONPayload, customShadowCallback_Update, 5)
+    loopCount += 1
+    time.sleep(1)
